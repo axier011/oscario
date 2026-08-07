45050021b7d57e6a716d3c4ce4a68e93bb02686c
Oscario <oscario@dao.local>
2026-08-07 09:38:08 +0200

    auto: 2026-08-07 09:38:08

---
+++ ojo_display.sh
@@ -0,0 +1,8 @@
+﻿#!/bin/bash
+DIR="$(dirname "$(realpath "$0")")"
+PYTHON="$DIR/venv/bin/python"
+SCRIPT="$DIR/ojo_animado.py"
+GIF1="$DIR/ojo.gif"
+GIF2="$DIR/ojo 2.gif"
+echo "[ojo-display] Pre-cargando ambos GIFs..."
+exec "$PYTHON" "$SCRIPT" "$GIF1" "$GIF2"
